4e1c58bb9d5638cfd522ff37c23d3794a80cee4a
Yi Kong <yikong@google.com>
2024-05-30 16:38:54 +0900

    profcollect: Do not collect traces for 32-bit libraries
    We have deprecated all AFDO optimizations for 32-bit libraries, no need
    to collect traces for these libraries.
    
    Test: presubmit
    Change-Id: I98f1763632981de6b4986d2fd3783857d8a23543

--- profcollectd/libprofcollectd/config.rs
+++ profcollectd/libprofcollectd/config.rs
@@ -31,7 +31,7 @@ use std::time::Duration;
 const PROFCOLLECT_CONFIG_NAMESPACE: &str = "aconfig_flags.profcollect_native_boot";
 const PROFCOLLECT_NODE_ID_PROPERTY: &str = "persist.profcollectd.node_id";
 
-const DEFAULT_BINARY_FILTER: &str = "(^/(system|apex/.+|vendor)/(bin|lib|lib64)/.+)|\
+const DEFAULT_BINARY_FILTER: &str = "(^/(system|apex/.+|vendor)/(bin|lib64)/.+)|\
     (^/data/app/.+\\.so$)|kernel.kallsyms";
 pub const REPORT_RETENTION_SECS: u64 = 14 * 24 * 60 * 60; // 14 days.
 
